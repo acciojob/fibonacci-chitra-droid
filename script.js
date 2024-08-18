e420df8834790fc4356dbf1eef3ebc74a16af38e
CHITRA <151122913+chitra-droid@users.noreply.github.com>
2024-08-18 10:41:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function fibonacci(num) {
-if(num===1){
-	return 0;
+// if(num===1){
+// 	return 0;
+// }
+// if(num===2){
+// 	return 1;
+// }
+// return fibonacci(num-1) + fibonacci(num-2);
+	let fib = [0, 1];
+    for(let i = 2; i <= num; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[num-1];
 }
-if(num===2){
-	return 1;
-}
-return fibonacci(num-1) + fibonacci(num-2);
-}
-console.log(fibonacci(5));
+console.log(fibonacci(7));
 
 
